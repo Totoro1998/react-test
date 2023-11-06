9c3046aed06161b26105dab6248d6ad4435e4074
totoro <totoro.fu@webeye.com>
2023-11-06 10:17:37 +0800

    test context

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// import { default as Test1 } from "./pages/state/preserving-and-resetting-state/Test1";
+// import Test from "./pages/state/preserving-and-resetting-state/Test2";
+
+import Test from "./pages/context/base/Test";
+
+function App() {
+  return <Test />;
+}
+
+export default App;
